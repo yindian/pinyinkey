9cc5f50e46a9e9ae45326d1822ba55ef9c98a1ab
yindian <yindian@c0fd70b7-8e33-0410-ae9c-9d2bdbf33376>
2007-07-02 03:02:28 +0000

    update

--- trunk/gui.py
+++ trunk/gui.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import sys, os, string, types, fileinput
+# author: YIN Dian	version: 20070701
+import sys, os, string, glob
 from Tkinter import *
 from tkFont import *
 from pyformat import *
@@ -15,7 +16,20 @@ class MainFrame(Frame):
 				underline=0)
 		self.menu_File.add_command(label='Exit', command=self.quit,
 				underline=1, accelerator='Alt-X')
+		self.menu_Edit = Menu(self.menu, tearoff=0)
+		self.menu.add_cascade(label='Edit', menu=self.menu_Edit,
+				underline=0)
+		def changenowword():
+			self.textbox.event_generate('<KeyPress-F5>')
+		self.menu_Edit.add_command(label='Change current word', 
+				command=changenowword, underline=0, 
+				accelerator='F5')
+		self.menu_Keyb = Menu(self.menu, tearoff=0)
+		self.menu.add_cascade(label='Keyboard', menu=self.menu_Keyb,
+				underline=0)
+		self.generatekeyboardmenu(self.menu_Keyb, '*.rule')
 		self._master["menu"] = self.menu
+		self._master.title('Pinyin Keyboard')
 		# hotkey
 		def quit(event):
 			self.quit()
@@ -29,6 +43,8 @@ class MainFrame(Frame):
 		self.textbox.pack(side=LEFT, expand=TRUE, fill=BOTH)
 		self.textbox_yscroll.pack(side=RIGHT, fill=Y)
 		self.textbox.bind('<KeyRelease>', self.textbox_keyrelease)
+		self.textbox.bind('<KeyPress-Return>', self.textbox_onreturn)
+		self.textbox.bind('<KeyPress-F5>', self.textbox_donowword)
 		self.textbox.focus_set()
 	def __init__(self, master=None):
 		Frame.__init__(self, master)
@@ -39,6 +55,23 @@ class MainFrame(Frame):
 		self.lastpos = ''
 		self.lastchar = u''
 		self.changed = False
+	def generatekeyboardmenu(self, menu, pattern):
+		self.nowkbd = IntVar()
+		self.nowkbd.set(0)
+		i = 0
+		pathprefix = '.'+os.sep+os.path.dirname(sys.argv[0])+os.sep
+		filelist = glob.glob(pathprefix+pattern)
+		filelist.sort()
+		for fname in filelist:
+			def selectrule(fname=fname, i=i):
+				specifyrule(fname)
+				print fname, 'selected'
+				self.nowkbd.set(i)
+			menu.add_radiobutton(label=`i`+'. '+fname, 
+					command=selectrule, value=i,
+					underline=0, variable=self.nowkbd)
+			i += 1
+		menu.invoke(0)
 	def textbox_keyrelease(self, event):
 		#if event.keysym_num in range(32, 128):
 		#	print "'"+event.keysym+"'",
@@ -46,25 +79,56 @@ class MainFrame(Frame):
 		#	print event.keysym_num,
 		currentline = event.widget.get('insert linestart', 'insert lineend')
 		currentpos = event.widget.index('insert - 1 chars')
+		#currentpos = event.widget.index('insert')
 		char = event.widget.get('insert - 1 chars')
 		if event.keysym_num in range(32, 128) and char == event.char:
 			if char in delimitrset:
-				#i = len(currentline)-2
-				#while i >= 0 and currentline[i] in delimitrset:
-				#	i -= 1
-				#while i >= 0 and not currentline[i] in delimitrset:
-				#	i -= 1
-				#if i < 0:
-				#	i = 0
-				#nowword = currentline[i:]
-				#j = currentpos.index('.') + 1
-				#nowwordpos = currentpos[:j] + `i`
-				#print nowword, nowwordpos,
-				#print '>', currentline, '=', 
-				currentline = pyformat(currentline)
-				#print currentline,
-				event.widget.delete('insert linestart', 'insert lineend')
-				event.widget.insert(currentpos, currentline)
+				j = currentpos.index('.') + 1
+				i = k = int(currentpos[j:])
+				while i >= 0 and currentline[i] in delimitrset:
+					i -= 1
+				while i >= 0 and not currentline[i] in delimitrset:
+					i -= 1
+				if i < 0:
+					i = 0
+				nowword = currentline[i:k]
+				nowwordpos = currentpos[:j] + `i`
+				print nowword.encode('gbk', 'replace'), nowwordpos,
+				nowword = pyformat(nowword)
+				print
+				event.widget.delete(nowwordpos, currentpos)
+				event.widget.insert(nowwordpos, nowword)
+				##print '>', currentline, '=', 
+				#currentline = pyformat(currentline)
+				##print currentline,
+				#event.widget.delete('insert linestart', 'insert lineend')
+				#event.widget.insert('insert', currentline)
+	def textbox_onreturn(self, event):
+		currentline = event.widget.get('insert linestart', 'insert lineend')
+		currentpos = event.widget.index('insert - 1 chars')
+		currentline = pyformat(currentline)
+		event.widget.delete('insert linestart', 'insert lineend')
+		event.widget.insert(currentpos, currentline)
+	def textbox_donowword(self, event):
+		currentline = event.widget.get('insert linestart', 'insert lineend')
+		currentpos = event.widget.index('insert')
+		j = currentpos.index('.') + 1
+		i = k = int(currentpos[j:])
+		if i == len(currentline):
+			i -= 1
+		while i >= 0 and currentline[i] in delimitrset:
+			i -= 1
+		while i >= 0 and not currentline[i] in delimitrset:
+			i -= 1
+		if i < 0:
+			i = 0
+		nowword = currentline[i:k]
+		nowwordpos = currentpos[:j] + `i`
+		print nowword.encode('gbk', 'replace'), nowwordpos,
+		nowword = pyformat(nowword)
+		print
+		event.widget.delete(nowwordpos, currentpos)
+		event.widget.insert(nowwordpos, nowword)
 
 if __name__ == '__main__':
 	root = Tk()
